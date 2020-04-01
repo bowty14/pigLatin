00eecc4ef2b02b284f33d73469a5196fed668454
Tyler Bates <Fondlytyler@gmail.com>
2020-04-01 14:33:08 -0700

    add spaces to input pattern so multiple words may be registered.

--- js/scripts.js
+++ js/scripts.js
@@ -1,21 +1,37 @@
 // Business Logic //
 var translate = function(str) {
-  str = str.toLowerCase();
+  str = str.toLowerCase().split("");
   if (["a", "e", "i", "o", "u"].indexOf(str[0]) > -1) {
     return str = str + "way";
-  }
+  } // else {
+  //   for (var i=0; i<str.length; i++) {
+  //     if (["a", "e", "i", "o", "u"].indexOf(str[i]) > -1) {
+  //       var firstcons = str.slice(0, i);
+  //       var middle = str.slice(i,str.length);
+  //       str = middle+firstcons+"ay";
+  //       break;
+  //     }
+  //     return str;
+  //   }
 };
     
+
+// var inputNew = str.split("");
+
+// For words beginning with one or more consonants, move all of the first consecutive consonants to the end, and add "ay". help me out / elphay emay out
 
 // User Interface Logic //
 $(document).ready(function() {
   $("form#pigLatin").submit(function(event) {
     event.preventDefault();
     var str = $("input#str").val();
+    // var inputNew = str.split("");
+
+    
     var result = translate(str);
     if (result) {
     $(".return").text(result);
     }
-    console.log(str);
+    // console.log(result);
   });
 });
